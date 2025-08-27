9d0c0caf4523881abad7c2627f79ec9a649dabad
Ronak <2022ucp1729@mnit.ac.in>
2025-08-27 19:09:51 +0530

    test

--- server/controllers/webhook.mjs
+++ server/controllers/webhook.mjs
@@ -10,7 +10,7 @@ export const clerkWebhooks=async (req,res)=>{
       "svix-timestamp":req.headers["svix-timestamp"],
       "svix-signature":req.headers["svix-signature"]
     })
-
+    const {data,type}=req.body 
     switch(type){
       case 'user.created':{
         const userData={
@@ -45,7 +45,7 @@ export const clerkWebhooks=async (req,res)=>{
         break;
     }
 
-    const {data,type}=req.body
+    
   } catch (error) {
     res.json({success:false,message:error.message})
 
